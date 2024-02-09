d5d07e8696eb63d01680a58f7ca35123b6a1b86a
jamesmcdonagh <jamesmcdonagh@monzo.com>
2024-02-09 15:41:39 +0000

    add cmt

--- default.go
+++ default.go
@@ -113,6 +113,10 @@ func Trace(ctx context.Context, msg string, params ...interface{}) {
 	}
 }
 
+// FromError constructs a logging event with error severity by default.
+// If the default Logger implements the FromErrorLogger interface, we
+// forward the requests via the FromError interface function. In this
+// case the severity will be inferred from the error.
 func FromError(ctx context.Context, msg string, err error, params ...interface{}) {
 	if l := DefaultLogger(); l != nil {
 		params = []interface{}{err, params}
