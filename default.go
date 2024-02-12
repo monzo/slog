4c6f6c2be05ef525c6c4c92fc2b992032c5d7114
James McDonagh <39272614+jdmcdonagh@users.noreply.github.com>
2024-02-12 15:19:17 +0000

    When using `FromError` then prepend the error to the params slice (#20)

--- default.go
+++ default.go
@@ -119,10 +119,10 @@ func Trace(ctx context.Context, msg string, params ...interface{}) {
 // case the severity will be inferred from the error.
 func FromError(ctx context.Context, msg string, err error, params ...interface{}) {
 	if l := DefaultLogger(); l != nil {
-		params = []interface{}{err, params}
 		if ll, ok := l.(FromErrorLogger); ok {
 			ll.FromError(ctx, msg, err, params...)
 		} else {
+			params = append([]interface{}{err}, params...)
 			l.Log(Eventf(ErrorSeverity, ctx, msg, params...))
 		}
 	}
